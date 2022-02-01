b97a91068bc182b96dae7c9b1b9c09df8ba378e5
Jeremiah <JeremiahBerndt@gmail.com>
2022-02-01 12:47:20 -0700

    header virtually complete

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,10 +2,7 @@ export default function Contact() {
   return (
     <div>
       <h3>Contact</h3>
-      {/* <h3>
-        <a href="mailto:jeremiahberndt@gmail.com">Jeremiah</a>
-      </h3> */}
-      {/* <a href="https://www.linkedin.com/in/jeremiah-berndt/">LinkedIn</a> */}
+
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,19 @@ import "./Header.scss";
 
 import myFace from "../../assets/images/myFace.png";
 import gitHubIcon from "../../assets/icons/GitHub-Mark-64px.png";
+import linkedInIcon from "../../assets/icons/LI-In-Bug.png";
+import envelopeIcon from "../../assets/icons/envelope-solid.svg";
 
-export interface IHeaderProps {
-  name: String;
-  size: Number;
-  className?: string;
-}
+// export interface IHeaderProps {
+//   name: String;
+//   size: Number;
+//   className?: string;
+// }
 
-export interface IExtendedHeader extends IHeaderProps {
-  poop: () => void;
-  pee: String[];
-}
+// export interface IExtendedHeader extends IHeaderProps {
+//   poop: () => void;
+//   pee: String[];
+// }
 
 const Header = () => {
   // const [test, setTest] = useState<String | null>(null)
@@ -31,12 +33,42 @@ const Header = () => {
           Jeremiah <span>Berndt</span>
         </h1>
         <h3>Software Engineer</h3>
-        <div>
-          <a href={gitHubIcon}>
-            <img
-              src="https://github.com/JeremiahBerndt"
-              alt="GitHub Profile"
-            ></img>
+        <div className="socialLinks">
+          {/* email */}
+
+          <a href="mailto:jeremiahberndt@gmail.com">
+            <img src={envelopeIcon} alt="link-to-email"></img>
+          </a>
+          {/* github */}
+          <a href="https://github.com/JeremiahBerndt">
+            <img src={gitHubIcon} alt="GitHub Profile"></img>
+          </a>
+          {/* linkedin */}
+          <div
+            className="badge-base LI-profile-badge"
+            data-locale="en_US"
+            data-size="medium"
+            data-theme="dark"
+            data-type="VERTICAL"
+            data-vanity="jeremiah-berndt"
+            data-version="v1"
+          >
+            <a
+              className="badge-base__link LI-simple-link"
+              href="https://www.linkedin.com/in/jeremiah-berndt?trk=profile-badge"
+            >
+              <img src={linkedInIcon} alt="LinkedIn Profile"></img>
+            </a>
+          </div>
+          {/* twitter */}
+          <a
+            href="https://twitter.com/JeremiahBerndt?ref_src=twsrc%5Etfw"
+            className="twitter-follow-button"
+            data-size="large"
+            data-show-screen-name="false"
+            data-show-count="false"
+          >
+            Follow @JeremiahBerndt
           </a>
         </div>
       </div>
