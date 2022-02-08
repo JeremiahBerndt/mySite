7ddf393e17ce7a8413beb5c26fa571f5f6482ee7
Jeremiah <JeremiahBerndt@gmail.com>
2022-02-08 08:19:21 -0700

    project setup

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Project() {
+  return (
+    <div className="project">
+      <div>
+        <h2>BiteShare</h2>
+      </div>
+    </div>
+  );
+}
